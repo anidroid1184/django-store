9e17c56fd3d1a65ba1cb9f8995f51d6124740930
Juan Sebastian Valencia Londoño <valencialondonojuansebastian@gmail.com>
2025-08-01 10:45:50 -0500

    Actualización de funcionalidades del carrito
    Se agrego una verificación de usuario previo al carrito, si el usuario entra como visitante anónimo  y después se inicia sesión, se mantendrá el carrito que tenía

--- landing/carts/models.py
+++ landing/carts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from users.models import User
 from products.models import Product
-from django.db.models.signals import pre_saves
+from django.db.models.signals import pre_save
+import uuid
 
 
 # Create your models here.
@@ -34,3 +35,21 @@ class Cart(models.Model):
     def __str__(self):
         # retornamos el id del carrito
         return self.cart_id
+    
+
+def set_cart_id(sender, instance, *args, **kwargs):
+    """
+    Función que agrega un valor unico a un visitante anonimo
+    para preservar su carrito
+    param sender: Modelo que envia la señal(Cart)
+    param instance: Instancia del modelo que envio la señal
+    param args: Argumentos adicionales
+    param kwargs: Argumentos adicionales
+    """
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+
+# Conectamos la señal pre_save al modelo Cart
+# para que se ejecute la función set_cart_id antes de guardar
+pre_save.connect(set_cart_id, sender=Cart)
--- landing/carts/views.py
+++ landing/carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cart
 
 
 # Create your views here.
@@ -6,4 +7,25 @@ def cart(request):
     # request.session.set_expiry(300)  # set 5 minutos de sessión
     # key = request.session.session_key
     # print(f'Session Key: {key}')
+    """
+    Despliega la vista del carrito
+    verifica el usuario, en caso de no estar autenticado,
+    retorna None
+    :param request: HttpRequest
+    :return: Renderiza la plantilla del carrito
+    """
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    cart = Cart.objects.filter(id=cart_id).first()
+
+    # si no hay carrito previo, se creara uno
+    if cart is None:
+        cart = Cart.objects.create(user=user)
+
+    # si el usuario se logea, se mantiene el carrito
+    if user and cart.user is None:
+        cart.user = user
+        cart.save()
+
+    request.session['cart_id'] = cart.cart_id
     return render(request, 'carts/carts.html', {})
